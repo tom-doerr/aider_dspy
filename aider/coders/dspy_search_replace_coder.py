750b51b398c33bdb0e708c6e17f29a8a71a2183a
Tom Dörr <tomdoerr96@gmail.com>
2024-12-13 07:08:20 +0100

    refactor: simplify get_edits method in DSPySearchReplaceCoder

--- aider/coders/dspy_search_replace_coder.py
+++ aider/coders/dspy_search_replace_coder.py
@@ -1,9 +1,11 @@
 from aider.coders.base_coder import Coder
 from aider.dspy_search_replace import DSPySearchReplaceModule
+from aider.coders.editblock_prompts import EditBlockPrompts
+
 
 class DSPySearchReplaceCoder(Coder):
     edit_format = "dspy-search-replace"
-    gpt_prompts = EditBlockPrompts()  # Set gpt_prompts during class definition
+    gpt_prompts = EditBlockPrompts()
 
     def __init__(self, main_model, io, **kwargs):
         super().__init__(main_model, io, **kwargs)
@@ -16,7 +18,8 @@ class DSPySearchReplaceCoder(Coder):
 
         return content.replace(search_text, replace_text)
 
-    def get_edits(self, fnames, content, edit_request):
+    # def get_edits(self, fnames, content, edit_request):
+    def get_edits(self):
         edits = {}
         for fname, file_content in zip(fnames, content):
             edit = self.dspy_search_replace.generate_search_replace(
